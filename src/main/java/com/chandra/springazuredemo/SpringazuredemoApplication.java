67271d60a9c4407defd52a4d1895cf1015669166
chandrasamanta <samanta.chandrakanta@gmail.com>
2021-10-22 17:26:18 +0530

    second commit

--- src/main/java/com/chandra/springazuredemo/SpringazuredemoApplication.java
+++ src/main/java/com/chandra/springazuredemo/SpringazuredemoApplication.java
@@ -2,12 +2,14 @@ package com.chandra.springazuredemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SpringazuredemoApplication {
 
+	@GetMapping("/message")
 	public String message(){
 		return "Congratulations! Your App deployed successfully.";
 	}
